74ded2e06caa7ef765ae538df5f03892181beda5
Kartik4152 <gaming4152@gmail.com>
2021-03-04 21:10:14 +0530

    changed email

--- src/modal.js
+++ src/modal.js
@@ -4,7 +4,7 @@ const modal=(props)=>{
 return(
     <div className="modalContainer">
         <div className="modalText">By agreeing to use this site, you provide your consent to save your playlistURL for matching with other users.</div>
-        <div className="modalText">For data deletion please contact at gaming4152@gmail.com</div>
+        <div className="modalText">For data deletion please contact at thatsmyplaylistapp@gmail.com</div>
         <div className="modalButton" onClick={()=>props.visible(false)}>Close</div>
     </div>
 )
